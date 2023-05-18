f3eb14bb7bb4ce05c52dbbb99c2e4bd7a65d5110
MaksStar <130188360+MaksStar@users.noreply.github.com>
2023-05-18 14:54:37 +0200

    Add files via upload

---
+++ Coffee/machinedOne.java
@@ -0,0 +1,11 @@
+package Coffee;
+
+public class machinedOne {
+    public String cof1(String q){
+        q = "Эспрессо с добавлением воды";
+        return q;
+    }
+    public String cof2(String w){
+        w = "Эспрессо с добавлением вспененного молока";
+        return w;
+}
