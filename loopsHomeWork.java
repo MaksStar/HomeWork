bb531c33d43c2f5c42c7300ee2607b8f70db2107
MaksStar <130188360+MaksStar@users.noreply.github.com>
2023-05-25 01:13:18 +0200

    Add files via upload

---
+++ loopsHomeWork.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class loopsHomeWork {
+    public static void main(String[] args) {
+        int[] q = {5,3,2,8,1,4,9};
+
+        for (int i = 0; i < q.length; i++)
+            System.out.print(q[i] + 10 + " ");
+
+    }
+}
